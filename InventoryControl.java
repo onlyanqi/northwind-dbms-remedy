3fe01fbef0cb54a2a5f27abc3c13c2390c371127
Anqi Chen <an883236@dal.ca>
2020-04-06 23:54:20 -0300

    InventoryControl.java is added

---
+++ InventoryControl.java
@@ -0,0 +1,6 @@
+public interface InventoryControl 
+{
+	public void Ship_order( int orderNumber ) throws OrderException; 
+	public int Issue_reorders( int year, int month, int day );
+	public void Receive_order( int internal_order_reference ) throws OrderException;
+}
